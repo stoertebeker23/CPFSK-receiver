af9d8290ad57e767f34a307b5ac9b7239923e19e
stoertebeker23 <stoerte@posteo.net>
2021-06-12 16:01:30 +0200

    working decoder with little issues

--- loopback_starter_MSJ/include/decode.h
+++ loopback_starter_MSJ/include/decode.h
@@ -5,19 +5,6 @@
 #ifndef C_DECODE_H
 #define C_DECODE_H
 
-typedef struct {
-    unsigned short *const buffer;
-    short head;
-    short tail;
-    const short maxidx;
-} circ_buff_t;
-
-
-short shortFromBits(unsigned const short* bits, short len);
-void _decode_lookup(const unsigned short *bits);
-void _decode(const unsigned short *bits);
 void decode(unsigned short bit);
-void circ_buff_push(circ_buff_t *c, unsigned short data);
-
 
 #endif //C_DECODE_H
