fd037a0533a11356861995c8fb2167e0c2715ca4
tobiasfrahm <frahm.tobias@gmail.com>
2021-06-22 11:38:11 +0200

    rename lol -> coeff

--- python/filtersplit.py
+++ python/filtersplit.py
@@ -1,23 +1,24 @@
 import csv
+
 with open('../matlab/filter.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
        values = row[0].split(",")
-    lol = []
+    coeff = []
     for i in range (0,526+1):
-    	lol.append([])
+    	coeff.append([])
     for j in range(0,3+1):
 	    for i in range(0, 526+1):
 	    	try:
-	    		lol[i].append(values[i+j*527])
+	    		coeff[i].append(values[i + j * 527])
 	    	except:
 	    		print("fuggedi")
-	    		lol[i].append('0')
+	    		coeff[i].append('0')
 
-    print(lol)
+    print(coeff)
 
     for i in range (0,526+1):
     	print("const short FIR_BANDPASS_" + str(i+1) + "[4] = {", end = " ")
-    	for elem in lol[i]:
+    	for elem in coeff[i]:
     		print(str(elem), end = ", ")
     	print("};")
